c1b09f0c50baca81223b0f7739d84d6f80a6035a
eucharia854 <ekaaningeucharia8@gmail.com>
2025-02-15 09:14:32 -0800

    Music app funtionalities done

---
+++ src/main/java/com/mycompany/musicapp/MusicApp.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.musicapp;
+
+/**
+ *
+ * @author Dell
+ */
+import UI.LogIn;
+public class MusicApp {
+
+    public static void main(String[] args) {
+          LogIn log = new LogIn();
+        log.setVisible(true);
+    }
+}
